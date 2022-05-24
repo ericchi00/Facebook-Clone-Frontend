71a71f7f97e3773acaa0c146a81c37e5741a199d
ericchi00 <ericchi00@gmail.com>
2022-05-24 14:50:56 -0700

    setup auth

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { AuthProvider } from 'react-auth-kit';
+import RoutesComponent from './routes/RoutesComponent';
 
 const App = () => {
-	return <></>;
+	return (
+		<AuthProvider authType={'localstorage'} authName={'_auth'}>
+			<RoutesComponent />
+		</AuthProvider>
+	);
 };
 
 export default App;
