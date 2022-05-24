b6a798045cb0a7c81b2856eb7ad6d6f2353d06fc
ericchi00 <ericchi00@gmail.com>
2022-05-24 14:51:01 -0700

    footer

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
+const Footer = () => {
+	return (
+		<Navbar bg="dark" variant="dark" style={{ height: 'auto' }}>
+			<Container fluid className="justify-content-center">
+				<Navbar.Text>Built by Eric Chi.</Navbar.Text>
+				<Nav.Link href="">Backend API</Nav.Link>
+				<Nav.Link href="">Backend API Source Code</Nav.Link>
+			</Container>
+		</Navbar>
+	);
+};
+
+export default Footer;
