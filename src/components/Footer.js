d1b3e4763ac1ce337efb5c75e3b410b80aee8bf5
ericchi00 <ericchi00@gmail.com>
2022-05-27 15:49:40 -0700

    changed footer link color

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,14 +2,26 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import { ReactComponent as Github } from '../assets/github.svg';
 
 const Footer = () => {
 	return (
 		<Navbar bg="dark" variant="dark" className="w-100" fixed="bottom">
-			<Container fluid className="justify-content-center">
-				<Navbar.Text>Built by Eric Chi.</Navbar.Text>
-				<Nav.Link href="">Backend API</Nav.Link>
-				<Nav.Link href="">Backend API Source Code</Nav.Link>
+			<Container
+				fluid
+				className="justify-content-center"
+				style={{ color: '#fff' }}
+			>
+				<Github />
+				<Nav.Link
+					href="https://github.com/ericchi00"
+					style={{ color: 'white' }}
+				>
+					Eric Chi
+				</Nav.Link>
+				<Nav.Link href="" style={{ color: 'white' }}>
+					Source Code
+				</Nav.Link>
 			</Container>
 		</Navbar>
 	);
