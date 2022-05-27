04c8d4738ac6e02e27f1787f0b43a2c40a8a8c9c
ericchi00 <ericchi00@gmail.com>
2022-05-26 18:15:20 -0700

    added in react auth kit

---
+++ src/routes/RoutesComponent.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { RequireAuth, useIsAuthenticated } from 'react-auth-kit';
+import Header from '../Authenticated/Header';
+import Home from '../Authenticated/Home';
+import Profile from '../Authenticated/Profile';
+import Footer from '../components/Footer';
+import ErrorPage from '../components/ErrorPage';
+
+const RoutesComponent = () => {
+	const isAuthenticated = useIsAuthenticated();
+
+	return (
+		<>
+			<BrowserRouter basename="/facebook-clone-frontend">
+				{isAuthenticated() ? <Header /> : null}
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route
+						path="/profile/:id"
+						element={
+							<RequireAuth loginPath="/">
+								<Profile />
+							</RequireAuth>
+						}
+					/>
+					<Route path="*" element={<ErrorPage />} />
+				</Routes>
+			</BrowserRouter>
+			<Footer />
+		</>
+	);
+};
+
+export default RoutesComponent;
