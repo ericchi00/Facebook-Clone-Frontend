74ddfc0cc1665484e54f3e87e04658b9c975a690
ericchi00 <ericchi00@gmail.com>
2022-05-29 17:42:58 -0700

    added in white color for labels

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -5,7 +5,9 @@ const InputField = ({ label, ...props }) => {
 	const [field, meta] = useField(props);
 	return (
 		<>
-			<label htmlFor={props.id || props.name}>{label}</label>
+			<label className="text-light" htmlFor={props.id || props.name}>
+				{label}
+			</label>
 			<input className="text-input" {...field} {...props} />
 			{meta.touched && meta.error ? (
 				<div className="text-danger form-text">{meta.error}</div>
