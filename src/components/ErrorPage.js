6838d8fcdbae38e96743b2537fd9b1808a75f548
ericchi00 <ericchi00@gmail.com>
2022-05-29 17:43:05 -0700

    changed text to white

--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -7,9 +7,9 @@ const ErrorPage = () => {
 	document.title = 'Error 404';
 
 	return (
-		<Container fluid style={{ marginTop: 'auto', marginBottom: 'auto' }}>
-			<div className="text-center">
-				<h1 className="display-1 fw-bold">404</h1>
+		<Container fluid className="d-flex mt-5 pt-5 justify-content-center">
+			<div className="text-center text-light">
+				<h1 className="display-1 fw-bold text-light">404</h1>
 				<p className="fs-3">
 					<span className="text-danger">Opps!</span> Page not found.
 				</p>
