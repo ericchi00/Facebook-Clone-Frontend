628715cf7c0c79ea08504ad1d24083d462884b2c
ericchi00 <ericchi00@gmail.com>
2022-05-30 17:43:51 -0700

    removed jest import

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,7 +1,6 @@
 /**
  * @jest-environment jsdom
  */
-import { jest } from '@jest/globals';
 import { render, screen } from '@testing-library/react';
 import React from 'react';
 import App from '../src/App';
