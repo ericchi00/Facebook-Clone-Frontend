f471385b73d422d3e26629f1f8e3929e0cb734b6
ericchi00 <ericchi00@gmail.com>
2022-05-31 16:42:16 -0700

    fixed styling

--- src/Authenticated/PostForm.js
+++ src/Authenticated/PostForm.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
 
 const formStyle = {
 	resize: 'none',
 	border: 'none',
 	outline: 'none',
+	background: '#404040',
+	color: '#fff',
 };
 
 const PostForm = ({ auth, authHeader }) => {
@@ -16,8 +19,10 @@ const PostForm = ({ auth, authHeader }) => {
 	useEffect(() => {
 		if (post.length > 1) {
 			setDisabled(false);
+		} else {
+			setDisabled(true);
 		}
-	});
+	}, [post]);
 
 	const submitPost = async () => {
 		const postUserPost = await fetch(`/api/posts/${id}`, {
@@ -30,39 +35,41 @@ const PostForm = ({ auth, authHeader }) => {
 	};
 
 	return (
-		<Form
-			className="mt-4 rounded"
-			style={{ width: '600px', background: '#323232' }}
-		>
-			<Form.Group
-				className="m-3 p-3 d-flex align-items-center"
-				style={{ gap: '.5rem' }}
-				controlId="formStatusPost"
+		<Container fluid="sm mt-4" style={{ maxWidth: '600px' }}>
+			<Form
+				className="rounded d-flex flex-column p-3"
+				style={{ background: '#323232' }}
 			>
-				<Form.Label className="visually-hidden">Post</Form.Label>
-				<img
-					src={auth().picture}
-					alt="your profile"
-					width={50}
-					height={50}
-					className="rounded-circle pe-auto"
-				/>
-				<Form.Control
-					className="rounded"
-					as="textarea"
-					placeholder={`What's on your mind, ${auth().firstName}?`}
-					rows="4"
-					style={formStyle}
-					onChange={(e) => setPost(e.target.value)}
-					name="userPost"
-					minLength={1}
-					required
-				/>
+				<Form.Group
+					className="p-3 d-flex align-items-center"
+					style={{ gap: '.5rem' }}
+					controlId="formStatusPost"
+				>
+					<Form.Label className="visually-hidden">Post</Form.Label>
+					<img
+						src={auth().picture}
+						alt="your profile"
+						width={50}
+						height={50}
+						className="rounded-circle pe-auto"
+					/>
+					<Form.Control
+						className="rounded"
+						as="textarea"
+						placeholder={`What's on your mind, ${auth().firstName}?`}
+						rows="4"
+						style={formStyle}
+						onChange={(e) => setPost(e.target.value)}
+						name="userPost"
+						minLength={1}
+						required
+					/>
+				</Form.Group>
 				<Button variant="light" disabled={disabled} type="submit">
 					Post
 				</Button>
-			</Form.Group>
-		</Form>
+			</Form>
+		</Container>
 	);
 };
 
