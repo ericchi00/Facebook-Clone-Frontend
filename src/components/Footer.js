1b61ead14537f0de6286efd2d016c520d530aa01
ericchi00 <ericchi00@gmail.com>
2022-05-31 15:53:34 -0700

    conditional rendering for mobile

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,28 +2,47 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import useCheckMobileScreen from '../hooks/useCheckMobileScreen';
 import { ReactComponent as Github } from '../assets/github.svg';
 
 const Footer = () => {
+	const check = useCheckMobileScreen();
+
 	return (
-		<Navbar className="w-100" fixed="bottom" style={{ background: '#323232' }}>
-			<Container
-				fluid
-				className="justify-content-center"
-				style={{ color: 'white' }}
-			>
-				<Github />
-				<Nav.Link
-					href="https://github.com/ericchi00"
-					style={{ color: 'white' }}
+		<>
+			{!check && (
+				<Navbar
+					className="d-flex mt-auto justify-content-center"
+					style={{ background: '#323232' }}
 				>
-					Eric Chi
-				</Nav.Link>
-				<Nav.Link href="" style={{ color: 'white' }}>
-					Source Code
-				</Nav.Link>
-			</Container>
-		</Navbar>
+					<Container
+						fluid
+						className="justify-content-center"
+						style={{ color: 'white' }}
+					>
+						<Github />
+						<Nav.Link
+							href="https://github.com/ericchi00"
+							style={{ color: 'white' }}
+						>
+							Eric Chi
+						</Nav.Link>
+						<Nav.Link
+							href="https://github.com/ericchi00/Facebook-Clone-Frontend"
+							style={{ color: 'white' }}
+						>
+							Source Code
+						</Nav.Link>
+						<Nav.Link
+							href="https://github.com/ericchi00/Facebook-Clone-Backend"
+							style={{ color: 'white' }}
+						>
+							Backend Source Code
+						</Nav.Link>
+					</Container>
+				</Navbar>
+			)}
+		</>
 	);
 };
 
