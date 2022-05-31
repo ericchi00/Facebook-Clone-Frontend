36a55d9e27aaff6c72cbe9dedeafb9191a60f26f
ericchi00 <ericchi00@gmail.com>
2022-05-30 17:45:09 -0700

    added in drop down bar

--- src/Authenticated/Header.js
+++ src/Authenticated/Header.js
@@ -1,44 +1,63 @@
 import React from 'react';
 import { useSignOut } from 'react-auth-kit';
 import { Link, useNavigate } from 'react-router-dom';
-import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
-import Button from 'react-bootstrap/Button';
-import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { ReactComponent as Profile } from '../assets/profile.svg';
 import { ReactComponent as FaceBookIcon } from '../assets/facebook.svg';
 
 const Header = ({ auth }) => {
 	const fullName = auth().firstName + ' ' + auth().lastName;
 	const id = auth().id;
-
 	const navigate = useNavigate();
 	const signOut = useSignOut();
 
 	return (
-		<Navbar className="w-100" style={{ background: '#30475E' }}>
-			<Container fluid>
+		<Navbar style={{ background: '#30475E' }}>
+			<Container fluid="xxl">
 				<Navbar.Brand as={Link} to="/" style={{ color: '#fff', gap: '.5rem' }}>
 					<FaceBookIcon />
 					<Navbar.Text style={{ paddingLeft: '1rem', color: '#fff' }}>
 						Facebook Clone
 					</Navbar.Text>
 				</Navbar.Brand>
-				<Nav className="me-auto"></Nav>
-				<Navbar.Text style={{ color: '#fff', paddingRight: '1rem' }}>
-					Signed in as:{' '}
-					<Link style={{ color: '#fff' }} to={`/profile/${id}`}>
-						{fullName}
-					</Link>
-				</Navbar.Text>
-				<Button
-					variant="light"
-					onClick={() => {
-						navigate('/');
-						signOut();
-					}}
+				<NavDropdown
+					title={<Profile />}
+					id="nav-dropdown-settings"
+					menuVariant="dark"
+					align="end"
 				>
-					Logout
-				</Button>
+					<NavDropdown.Item
+						as={Link}
+						to={`/profile/${id}`}
+						className="d-flex align-items-center"
+						style={{ gap: '.5rem' }}
+					>
+						<img
+							width={30}
+							height={30}
+							alt="profile pic"
+							src={auth().picture}
+							className="rounded-circle"
+						/>
+						<div className="d-flex flex-column">
+							{fullName}
+							<small>See your profile</small>
+						</div>
+					</NavDropdown.Item>
+					<NavDropdown.Item>Friends</NavDropdown.Item>
+					<NavDropdown.Item>Messages</NavDropdown.Item>
+					<NavDropdown.Item
+						className="w-auto"
+						onClick={() => {
+							navigate('/');
+							signOut();
+						}}
+					>
+						Log Out
+					</NavDropdown.Item>
+				</NavDropdown>
 			</Container>
 		</Navbar>
 	);
