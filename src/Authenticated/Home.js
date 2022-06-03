18b796e10365029f37b29dd3612ef5640939a2e5
ericchi00 <ericchi00@gmail.com>
2022-06-02 17:23:48 -0700

    refactor to use ternary

--- src/Authenticated/Home.js
+++ src/Authenticated/Home.js
@@ -1,13 +1,15 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Login from '../components/Login';
-import PostForm from './PostForm';
+import PostForm from './Post/PostForm';
 import UserInfo from './UserInfo';
 import Friends from './Friends/Friends';
+import Post from './Post/Post';
 import { useIsAuthenticated } from 'react-auth-kit';
 import Container from 'react-bootstrap/Container';
 import useCheckMobileScreen from '../hooks/useCheckMobileScreen';
 
 const Home = ({ auth, authHeader }) => {
+	const [posts, setPosts] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 	const checkMobile = useCheckMobileScreen();
 	const isAuthenticated = useIsAuthenticated();
 
@@ -15,27 +17,30 @@ const Home = ({ auth, authHeader }) => {
 		document.title = 'Facebook Clone';
 	});
 
-	if (isAuthenticated()) {
-		return (
-			<Container
-				fluid="lg"
-				className="d-flex justify-content-between"
-				style={{
-					gap: '1rem',
-				}}
-			>
-				{!checkMobile && <UserInfo auth={auth} />}
-				<PostForm auth={auth} authHeader={authHeader} />
-				{!checkMobile && <Friends auth={auth} authHeader={authHeader} />}
-			</Container>
-		);
-	} else {
-		return (
-			<>
-				<Login />
-			</>
-		);
-	}
+	return (
+		<>
+			{isAuthenticated() && (
+				<Container
+					fluid="lg"
+					className="d-flex justify-content-between"
+					style={{
+						gap: '1rem',
+					}}
+				>
+					{!checkMobile && <UserInfo auth={auth} />}
+					<div className="w-100">
+						<PostForm auth={auth} authHeader={authHeader} />
+						{posts.map((userInfo, i) => {
+							return <Post key={i} />;
+						})}
+					</div>
+
+					{!checkMobile && <Friends auth={auth} authHeader={authHeader} />}
+				</Container>
+			)}
+			{!isAuthenticated() && <Login />}
+		</>
+	);
 };
 
 export default Home;
