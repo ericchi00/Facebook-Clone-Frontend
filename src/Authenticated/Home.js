9d838072f7dd29d4c956c9315b411e9cadabab15
ericchi00 <ericchi00@gmail.com>
2022-06-04 16:27:39 -0700

    adding in loading spinner

--- src/Authenticated/Home.js
+++ src/Authenticated/Home.js
@@ -6,9 +6,11 @@ import Friends from './Friends/Friends';
 import Post from './Post/Post';
 import { useIsAuthenticated } from 'react-auth-kit';
 import Container from 'react-bootstrap/Container';
+import Spinner from 'react-bootstrap/Spinner';
 import useCheckMobileScreen from '../hooks/useCheckMobileScreen';
 
 const Home = ({ auth, authHeader }) => {
+	const [loading, setLoading] = useState(true);
 	const [posts, setPosts] = useState([]);
 	const checkMobile = useCheckMobileScreen();
 	const isAuthenticated = useIsAuthenticated();
@@ -31,40 +33,55 @@ const Home = ({ auth, authHeader }) => {
 		if (getPosts.status === 200) {
 			const response = await getPosts.json();
 			setPosts(response);
+			setLoading(false);
 		}
 	};
-
 	return (
 		<>
 			{isAuthenticated() && (
-				<Container
-					fluid="lg"
-					className="d-flex justify-content-between"
-					style={{
-						gap: '1rem',
-					}}
-				>
-					{!checkMobile && <UserInfo auth={auth} />}
-					<div className="w-100">
-						<PostForm
-							auth={auth}
-							authHeader={authHeader}
-							newPost={newPost}
-							setNewPost={setNewPost}
-						/>
-						{posts.map((postInfo) => {
-							return (
-								<Post
+				<>
+					{loading ? (
+						<Container className="d-flex align-items-center justify-content-center">
+							<Spinner
+								animation="border"
+								variant="light"
+								role="status"
+								style={{
+									width: '4rem',
+									height: '4rem',
+									marginTop: '10rem',
+								}}
+							>
+								<span className="visually-hidden">Loading...</span>
+							</Spinner>
+						</Container>
+					) : (
+						<Container fluid="lg" className="d-flex justify-content-between">
+							{!checkMobile && <UserInfo auth={auth} />}
+							<div className="w-100">
+								<PostForm
 									auth={auth}
 									authHeader={authHeader}
-									postInfo={postInfo}
-									key={postInfo._id}
+									newPost={newPost}
+									setNewPost={setNewPost}
 								/>
-							);
-						})}
-					</div>
-					{!checkMobile && <Friends auth={auth} authHeader={authHeader} />}
-				</Container>
+								{posts.map((postInfo) => {
+									return (
+										<Post
+											auth={auth}
+											authHeader={authHeader}
+											postInfo={postInfo}
+											key={postInfo._id}
+										/>
+									);
+								})}
+							</div>
+							{!checkMobile && (
+								<Friends auth={auth} authHeader={authHeader} width={'300px'} />
+							)}
+						</Container>
+					)}
+				</>
 			)}
 			{!isAuthenticated() && <Login />}
 		</>
