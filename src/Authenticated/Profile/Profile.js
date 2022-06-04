d2dbf73a3885decfe95c83e2e0b1615c564c4817
ericchi00 <ericchi00@gmail.com>
2022-06-04 16:55:03 -0700

    dispalys users posts

--- src/Authenticated/Profile/Profile.js
+++ src/Authenticated/Profile/Profile.js
@@ -2,17 +2,22 @@ import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Spinner from 'react-bootstrap/Spinner';
-import PostForm from '../Post/PostForm';
 import ProfileCard from './ProfileCard';
+import Post from '../Post/Post';
+import Friends from '../Friends/Friends';
+import useCheckMobileScreen from '../../hooks/useCheckMobileScreen';
 
 const Profile = ({ auth, authHeader }) => {
 	const { id } = useParams();
+	const navigate = useNavigate();
 	const [info, setInfo] = useState('');
 	const [loading, setLoading] = useState(true);
 	const [isUserProfile, setIsUserProfile] = useState(false);
 	const [profileChange, setProfileChange] = useState(false);
+	const [userPosts, setUserPosts] = useState([]);
 
-	const navigate = useNavigate();
+	const [style, setStyle] = useState();
+	const checkMobile = useCheckMobileScreen();
 
 	useEffect(() => {
 		setLoading(true);
@@ -39,7 +44,22 @@ const Profile = ({ auth, authHeader }) => {
 		const profileInfo = await response.json();
 		setInfo(profileInfo);
 		document.title = profileInfo.firstName + ' ' + profileInfo.lastName;
+		getUserPosts();
 		setLoading(false);
+	};
+
+	const getUserPosts = async () => {
+		const getPosts = await fetch(`/api/profile/${id}/posts`, {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: authHeader(),
+			},
+		});
+		if (getPosts.status === 200) {
+			const response = await getPosts.json();
+			setUserPosts(response);
+		}
 	};
 
 	return (
@@ -60,15 +80,40 @@ const Profile = ({ auth, authHeader }) => {
 					</Spinner>
 				</Container>
 			) : (
-				<>
-					<ProfileCard
-						auth={auth}
-						authHeader={authHeader}
-						isUserProfile={isUserProfile}
-						info={info}
-						setProfileChange={setProfileChange}
-					/>
-				</>
+				<Container
+					fluid="lg"
+					className={
+						!checkMobile
+							? 'd-flex justify-content-center'
+							: 'd-flex flex-column'
+					}
+					style={{ gap: '1rem' }}
+				>
+					<div>
+						<ProfileCard
+							auth={auth}
+							authHeader={authHeader}
+							isUserProfile={isUserProfile}
+							info={info}
+							setProfileChange={setProfileChange}
+						/>
+						<Friends auth={auth} authHeader={authHeader} width={'600px'} />
+					</div>
+					{userPosts.length > 0 && (
+						<div style={{ minWidth: '300px' }}>
+							{userPosts.map((postInfo) => {
+								return (
+									<Post
+										auth={auth}
+										authHeader={authHeader}
+										postInfo={postInfo}
+										key={postInfo._id}
+									/>
+								);
+							})}
+						</div>
+					)}
+				</Container>
 			)}
 		</>
 	);
