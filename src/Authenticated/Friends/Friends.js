11ce62c4011160ff3c71adacd86bd6c2dc99e10b
ericchi00 <ericchi00@gmail.com>
2022-06-04 16:27:30 -0700

    renders friend request only for logged in user

--- src/Authenticated/Friends/Friends.js
+++ src/Authenticated/Friends/Friends.js
@@ -2,24 +2,29 @@ import React, { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import FriendRequest from './FriendRequest';
 import FriendsList from './FriendsList';
+import { useParams, useLocation } from 'react-router-dom';
 
-const Friends = ({ auth, authHeader }) => {
+const Friends = ({ auth, authHeader, width }) => {
+	const location = useLocation();
+	const { id } = useParams();
 	const [rerender, setRerender] = useState(false);
 	return (
 		<Container
-			fluid="sm mt-4 d-flex flex-column"
+			fluid="sm mt-4 d-flex flex-column rounded"
 			style={{
-				maxWidth: '300px',
+				maxWidth: width,
 				background: '#323232',
 				height: 'fit-content',
 			}}
 		>
-			<FriendRequest
-				auth={auth}
-				authHeader={authHeader}
-				rerender={rerender}
-				setRerender={setRerender}
-			/>
+			{(location.pathname === '/' || id === auth().id) && (
+				<FriendRequest
+					auth={auth}
+					authHeader={authHeader}
+					rerender={rerender}
+					setRerender={setRerender}
+				/>
+			)}
 			<FriendsList auth={auth} authHeader={authHeader} rerender={rerender} />
 		</Container>
 	);
