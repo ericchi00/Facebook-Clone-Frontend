d5685d13a4e1ceafa0fff5d0e3aa1dcac8c14cf1
ericchi00 <ericchi00@gmail.com>
2022-06-04 20:31:24 -0700

    renders person's friend list

--- src/Authenticated/Friends/Friends.js
+++ src/Authenticated/Friends/Friends.js
@@ -5,19 +5,21 @@ import FriendsList from './FriendsList';
 import { useParams, useLocation } from 'react-router-dom';
 
 const Friends = ({ auth, authHeader, width }) => {
-	const location = useLocation();
 	const { id } = useParams();
+	const location = useLocation();
 	const [rerender, setRerender] = useState(false);
 	return (
 		<Container
-			fluid="sm mt-4 mb-4 d-flex flex-column rounded"
+			fluid="sm mt-4 d-flex flex-column rounded"
 			style={{
 				maxWidth: width,
 				background: '#323232',
 				height: 'fit-content',
 			}}
 		>
-			{(location.pathname === '/' || id === auth().id) && (
+			{(location.pathname === '/' ||
+				id === auth().id ||
+				location.pathname === '/people') && (
 				<FriendRequest
 					auth={auth}
 					authHeader={authHeader}
--- src/Authenticated/Friends/FriendsList.js
+++ src/Authenticated/Friends/FriendsList.js
@@ -1,16 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import Button from 'react-bootstrap/Button';
-import { Link } from 'react-router-dom';
+import { Link, useParams, useLocation } from 'react-router-dom';
 
 const FriendsList = ({ auth, authHeader, rerender }) => {
-	const id = auth().id;
+	let location = useLocation();
+	const { id } = useParams();
 	const [friends, setFriends] = useState([]);
+	const [userID, setUserID] = useState();
+
+	useEffect(() => {
+		if (location.pathname === '/' || location.pathname === '/people') {
+			setUserID(auth().id);
+		} else {
+			if (id !== undefined) setUserID(id);
+		}
+		getFriends();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [rerender, userID, id]);
 
 	const getFriends = async () => {
-		const getAllFriends = await fetch(`/api/friends/${id}`, {
+		const getAllFriends = await fetch(`/api/friends/${userID}`, {
 			method: 'GET',
 			headers: {
-				'Content-Type': 'application/json',
 				Authorization: authHeader(),
 			},
 		});
@@ -20,49 +31,46 @@ const FriendsList = ({ auth, authHeader, rerender }) => {
 		}
 	};
 
-	useEffect(() => {
-		getFriends();
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [rerender]);
-
 	return (
-		<div className="w-100 h-100 p-3 " style={{ background: '#323232' }}>
-			<h5 className="text-center text-light">Friends</h5>
-			{friends.length === 0 ? (
-				<p className="text-center text-light">No friends yet</p>
-			) : (
-				<div
-					className="d-flex flex-wrap justify-content-around"
-					style={{ gap: '.8rem' }}
-				>
-					{friends.map((friend, i) => {
-						return (
-							<Button
-								as={Link}
-								to={`/profile/${friend._id}`}
-								key={i}
-								className="d-flex p-2 text-light justify-content-center
+		<>
+			<div className="w-100 h-100 p-3 " style={{ background: '#323232' }}>
+				<h5 className="text-center text-light">Friends</h5>
+				{friends.length === 0 ? (
+					<p className="text-center text-light">No friends yet :(</p>
+				) : (
+					<div
+						className="d-flex flex-wrap justify-content-around"
+						style={{ gap: '.8rem' }}
+					>
+						{friends.map((friend, i) => {
+							return (
+								<Button
+									as={Link}
+									to={`/profile/${friend._id}`}
+									key={i}
+									className="d-flex p-2 text-light justify-content-center
 										align-items-center w-100"
-								style={{
-									background: '#404040',
-									gap: '.5rem',
-									border: 'none',
-								}}
-							>
-								<img
-									src={friend.picture}
-									alt={'profile of ' + friend.firstName}
-									height={32}
-									width={32}
-									className="rounded-circle"
-								/>
-								{friend.firstName + ' ' + friend.lastName}
-							</Button>
-						);
-					})}
-				</div>
-			)}
-		</div>
+									style={{
+										background: '#404040',
+										gap: '.5rem',
+										border: 'none',
+									}}
+								>
+									<img
+										src={friend.picture}
+										alt={'profile of ' + friend.firstName}
+										height={32}
+										width={32}
+										className="rounded-circle"
+									/>
+									{friend.firstName + ' ' + friend.lastName}
+								</Button>
+							);
+						})}
+					</div>
+				)}
+			</div>
+		</>
 	);
 };
 
